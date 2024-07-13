dac4c46c670ff3e29143e278e284b9295e201b1c
Dmitry <dm8343242@gmail.com>
2024-07-13 19:50:34 +0300

    lesson1:Task1+

--- lesson1/Task1+/Program.cs
+++ lesson1/Task1+/Program.cs
@@ -1,11 +1,4 @@
-﻿
-//Console.WriteLine("Hello, World!");
-
-int value = 100;
-Console.WriteLine(value);
-double d = 5.55;
-char c = '!';
-bool b = true;
-Console.WriteLine(d);
-Console.WriteLine(c);
-Console.WriteLine(b);
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
